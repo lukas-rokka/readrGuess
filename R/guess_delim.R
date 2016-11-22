87b83dc1bf0b5ad5b51f3ba1efd38a7a78a57a70
Lukas Lundstrom <lukas.rokka@gmail.com>
2016-11-22 11:56:59 +0100

    added read_guess() and fixes

--- R/guess_delim.R
+++ R/guess_delim.R
@@ -4,36 +4,40 @@
 #' @import dplyr
 #'
 #' @export
-guess_delim <- function(file, cases=delim_cases()) {
+guess_delim <- function(file, cases=delim_cases(), ...) {
 
-  cases <- cases %>% mutate(n_cols=0, n_nonchar_cols=0)
+  cases <- cases %>% mutate(n_cols=NA, n_nonchar_cols=NA)
 
 
   for (i in 1:nrow(cases)) {
     case <- cases[i, ]
     #cat("delim: ", case$delim, "\ndecimal_mark: ", case$decimal_mark, "\ngrouping_mark: ", case$grouping_mark, "\n\n")
 
-
-    df = tryCatch({
-      read_delim2(file, case$delim, decimal_mark = case$decimal_mark, grouping_mark = case$grouping_mark)
-    }, warning = function(w) {
-      tibble::tibble()
-    }, error = function(e) {
-      stop(e)
-    })
-
-    cases$n_cols[i] <- ncol(df)
-    cases$n_nonchar_cols[i] <- sum(lapply(df, class) %>% unlist() == "character")
-    #print(df)
+    #df = read_delim2(file, case$delim, decimal_mark = case$decimal_mark, grouping_mark = case$grouping_mark, ...)
+      #
+     a <- tryCatch({
+       df <- read_delim2(file, case$delim, decimal_mark = case$decimal_mark, grouping_mark = case$grouping_mark, ...)
+       cases$n_cols[i] <- ncol(df)
+       cases$n_nonchar_cols[i] <- sum(lapply(df, class) %>% unlist() == "character")
+     }, warning = function(w) {
+       # cases with warnings are not rectanguler (data.frames)
+       # and therefore  considered as non-likely
+     }, error = function(e) {
+       stop(e)
+     })
   }
-
-  # cases with warnings are not rectanguler (data.frames)
-  # and therefore  considered as non-likely
-  if (any(cases$n_nonchar_cols==0))
-    cases[cases$n_nonchar_cols==0,]$n_nonchar_cols <- Inf
 
   # choose cases with least non-character columns and most total number of columns
   cases %>% arrange(n_nonchar_cols, desc(n_cols)) #%>% slice(1:1)
+}
+
+#' Read flat file by guessing the formating
+#'
+#' @export
+read_guess <- function(file, ...) {
+  guess <- guess_delim(file, ...)
+  print(guess)
+  read_delim2(file, guess$delim[1], guess$decimal_mark[1], guess$grouping_mark[1], ...)
 }
 
 #' read_delim2
